9276d11c5dbd92b8f50485930fe21418adb59aac
pengttyy <pengttyy@gmail.com>
2017-03-22 22:07:28 +0800

    init(project):初始化项目

---
+++ src/test/java/com/pengttyy/DbunitTest.java
@@ -0,0 +1,19 @@
+package com.pengttyy;
+
+import org.junit.Test;
+import org.unitils.UnitilsJUnit4;
+import org.unitils.dbunit.annotation.DataSet;
+
+import static org.unitils.reflectionassert.ReflectionAssert.assertPropertyLenientEquals;
+
+/**
+ * Created by pengt on 2017/3/22.
+ */
+//@DataSet
+public class DbunitTest extends UnitilsJUnit4 {
+    @Test
+    public void testFindByName() throws Exception {
+
+        //assertPropertyLenientEquals("", "", "");
+    }
+}
