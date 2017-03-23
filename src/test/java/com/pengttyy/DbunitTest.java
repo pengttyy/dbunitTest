3a97ec64e547f8b21a0283251c546eb5b8880264
pengttyy <pengttyy@gmail.com>
2017-03-24 00:20:41 +0800

    test(project):集成hsqldb数据库

--- src/test/java/com/pengttyy/DbunitTest.java
+++ src/test/java/com/pengttyy/DbunitTest.java
@@ -4,16 +4,14 @@ import org.junit.Test;
 import org.unitils.UnitilsJUnit4;
 import org.unitils.dbunit.annotation.DataSet;
 
-import static org.unitils.reflectionassert.ReflectionAssert.assertPropertyLenientEquals;
-
 /**
  * Created by pengt on 2017/3/22.
  */
-//@DataSet
+@DataSet
 public class DbunitTest extends UnitilsJUnit4 {
+
     @Test
     public void testFindByName() throws Exception {
-
-        //assertPropertyLenientEquals("", "", "");
+        System.out.println("test");
     }
 }
